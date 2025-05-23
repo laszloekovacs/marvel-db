1e32685a64b9a17b3d743b3abd8f7675486bf03f
laszloekovacs <laszloekovacs@gmail.com>
2025-05-23 15:58:14 +0200

    clean old deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
 	server: {
-		port: 3000
-	}
-})
+		port: 3000,
+	},
+});
