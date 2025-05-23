3b9c06d1677aa60966082fdd8ef23eeb695759d9
laszloekovacs <laszloekovacs@gmail.com>
2025-05-23 16:08:27 +0200

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+	plugins: [
+		react(),
+		TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+	],
+
 	server: {
 		port: 3000,
 	},
